6e3cb7d5b5500c09033d4dc2aecf98ebbd7ad1b5
鲁大光 <lujingzhezhe@outlook.com>
2017-12-21 00:00:05 +0800

    创建一些activity

---
+++ app/src/main/java/com/light/national_geographic/ui/activity/CollectionActivity.kt
@@ -0,0 +1,13 @@
+package com.light.national_geographic.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.light.national_geographic.R
+
+class CollectionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_collection)
+    }
+}
---
+++ app/src/main/java/com/light/national_geographic/ui/adapter/CollectionAdapter.kt
@@ -0,0 +1,10 @@
+package com.light.national_geographic.ui.adapter
+
+/**
+ * 创建日期：2017/12/20 on 23:59
+ * @author ludaguang
+ * @version 1.0
+ * 类说明：
+ */
+class CollectionAdapter {
+}
