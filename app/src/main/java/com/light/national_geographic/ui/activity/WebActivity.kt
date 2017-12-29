8c9e59a11b8e778fd75f16e9f9ebfca69c97487d
鲁大光 <lujingzhezhe@outlook.com>
2017-12-29 22:52:22 +0800

    webActivity创建

--- app/src/main/java/com/light/national_geographic/ui/activity/WebActivity.kt
+++ app/src/main/java/com/light/national_geographic/ui/activity/WebActivity.kt
@@ -1,13 +1,17 @@
 package com.light.national_geographic.ui.activity
 
+import android.databinding.DataBindingUtil.setContentView
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.light.national_geographic.R
+import com.light.national_geographic.databinding.ActivityWebBinding
 
-class WebActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_web)
+class WebActivity : BaseActiviy<ActivityWebBinding>() {
+    override val layoutId: Int
+        get() = R.layout.activity_web
+    override fun initView(savedInstanceState: Bundle?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
+
 }
