dd239dac41dfca20a9f1febe70095deb9740bba7
鲁大光 <lujingzhezhe@outlook.com>
2018-01-03 00:30:27 +0800

    收藏界面完成
    实现收藏界面adapter,点击事件

--- app/src/main/java/com/light/national_geographic/ui/activity/CollectionActivity.kt
+++ app/src/main/java/com/light/national_geographic/ui/activity/CollectionActivity.kt
@@ -1,14 +1,62 @@
 package com.light.national_geographic.ui.activity
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.view.View
 import com.light.national_geographic.R
+import com.light.national_geographic.data.Collects
+import com.light.national_geographic.data.model.Detail
 import com.light.national_geographic.databinding.ActivityDetailBinding
+import com.light.national_geographic.ui.adapter.CollectionAdapter
+import kotlinx.android.synthetic.main.activity_collection.*
 
-class CollectionActivity : BaseActiviy<ActivityDetailBinding>() {
+class CollectionActivity : BaseActiviy<ActivityDetailBinding>(), CollectionAdapter.OnItemClickListener, CollectionAdapter.OnCollectionClickListener {
+    var mAdapter: CollectionAdapter = CollectionAdapter(this)
+    var mData: Detail? = null
+    private var REQUEST_INFO = 100
+
+    override fun onItemClick(adapter: CollectionAdapter, position: Int, view: View, collectionViewHolder: CollectionAdapter.CollectionViewHolder, data: Detail) {
+        var intent: Intent = Intent(this, DetailActivity::class.java)
+        intent.putExtra("DETAIL", Collects.getDetail())
+        intent.putExtra("POS", position)
+        startActivity(intent)
+    }
+
+    override fun onItemClick(adapter: CollectionAdapter, position: Int, view: View, collectionAdapter: CollectionAdapter, data: Detail) {
+        Collects.deleteItem(data.picture?.get(position)?.id)
+        mAdapter.setData(Collects.getDetail())
+        if (mAdapter.itemCount == 0) {
+            finish()
+        }
+    }
+
     override val layoutId: Int
         get() = R.layout.activity_collection
 
     override fun initView(savedInstanceState: Bundle?) {
+        mData = this.intent.extras.getSerializable("DETAIL") as Detail
+        if (mData != null) {
+            mAdapter.setData(mData!!)
+        }
+        recycler_collection.adapter = mAdapter
+        recycler_collection.layoutManager = LinearLayoutManager(this)
+        mAdapter.setOnCollectionClickListener(this)
+        mAdapter.setOnItemClickListener(this)
+        back_collection.setOnClickListener({
+            onBackPressed()
+        })
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (REQUEST_INFO == requestCode && resultCode == Activity.RESULT_OK) {
+            mAdapter.setData(Collects.getDetail())
+        }
+        if (mAdapter.itemCount == 0) {
+            finish()
+        }
     }
 
 
--- app/src/main/java/com/light/national_geographic/ui/adapter/CollectionAdapter.kt
+++ app/src/main/java/com/light/national_geographic/ui/adapter/CollectionAdapter.kt
@@ -1,8 +1,13 @@
 package com.light.national_geographic.ui.adapter
 
 import android.content.Context
+import android.databinding.DataBindingUtil
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.light.national_geographic.R
 import com.light.national_geographic.data.model.Detail
 import com.light.national_geographic.databinding.ItemCollectionBinding
 
@@ -14,19 +19,20 @@ import com.light.national_geographic.databinding.ItemCollectionBinding
  */
 class CollectionAdapter(context: Context) : RecyclerView.Adapter<CollectionAdapter.CollectionViewHolder>() {
     var mData: Detail? = null
-    private var mContext:Context?=null
+    private var mContext: Context? = null
+
     init {
-        mContext=context
+        mContext = context
     }
 
-    fun setData(data:Detail){
-        mData=data
+    fun setData(data: Detail) {
+        mData = data
         notifyDataSetChanged()
     }
 
 
-    fun removeItem(position: Int){
-        if (mData!=null){
+    fun removeItem(position: Int) {
+        if (mData != null) {
             mData!!.picture!!.removeAt(position)
         }
         notifyDataSetChanged()
@@ -34,14 +40,27 @@ class CollectionAdapter(context: Context) : RecyclerView.Adapter<CollectionAdapt
 
 
     override fun onBindViewHolder(holder: CollectionViewHolder?, position: Int) {
+        Glide.with(mContext).load(mData?.picture?.get(position)?.url).into(holder?.binding?.imgCollection)
+        holder?.binding?.imgCollection?.setOnClickListener({view ->
+            if (mOnItemClickLisener!=null){
+                mOnItemClickLisener!!.onItemClick(this,position,view,holder!!,mData!!)
+            }
+        })
+        holder?.binding?.collect?.setOnClickListener({view ->
+            if (mOnItemClickLisener!=null){
+                mOnItemClickLisener!!.onItemClick(this,position,holder!!.binding!!.collect,holder!!,mData!!)
+            }
+        })
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): CollectionViewHolder {
+        val binding=DataBindingUtil.inflate<ItemCollectionBinding>(LayoutInflater.from(parent?.context), R.layout.item_collection,parent,false)
+        return  CollectionAdapter.CollectionViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
-        if (mData!=null ){
+        if (mData != null) {
             return mData!!.picture!!.size
         }
         return 0
@@ -55,5 +74,27 @@ class CollectionAdapter(context: Context) : RecyclerView.Adapter<CollectionAdapt
             this.binding = binding
         }
 
+
+
+    }
+
+    private var mOnItemClickLisener: OnItemClickListener? = null
+
+    interface OnItemClickListener {
+        fun onItemClick(adapter: CollectionAdapter, position: Int, view: View, collectionViewHolder: CollectionViewHolder, data: Detail)
+    }
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        mOnItemClickLisener = listener;
+    }
+
+    private var mOnCollectiongClickLisener: OnCollectionClickListener? = null
+
+    interface OnCollectionClickListener {
+        fun onItemClick(adapter: CollectionAdapter, position: Int, view: View, collectionAdapter: CollectionAdapter, data: Detail)
+    }
+
+    fun setOnCollectionClickListener(listener: OnCollectionClickListener) {
+        mOnCollectiongClickLisener = listener;
     }
 }
